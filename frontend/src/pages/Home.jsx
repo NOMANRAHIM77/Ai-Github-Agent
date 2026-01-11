97b9640a110a4ee08a345c5aaa3d89615f75987a
NOMANRAHIM77 <nomanrahim53@gmail.com>
2026-01-11 21:28:47 +0500

    updated home.jsx

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,23 +1,26 @@
 import { useState } from "react";
 import Header from "../components/Header";
 import ChatBox from "../components/ChatBox";
-
+import Loader from "../components/Loader";
 
 const Home = () => {
-  const [showReminder, setShowReminder] = useState(false);
+  const [loading, setLoading] = useState(true);
+
+
+  if (loading) {
+    return <Loader onFinish={() => setLoading(false)} />;
+  }
 
   return (
     <div className="h-screen bg-gray-900 text-white flex flex-col overflow-hidden">
       
       {/* Header */}
-      <Header  />
+      <Header onReminderClick={() => setShowReminder(!showReminder)} />
 
-
-      {/* Chat (scrolls independently) */}
+      {/* Chat area (only this scrolls) */}
       <div className="flex-1 overflow-hidden">
         <ChatBox />
       </div>
-
     </div>
   );
 };
