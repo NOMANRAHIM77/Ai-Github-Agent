09a8616405981a286aa49ea9cca6370bfa689f2c
NOMANRAHIM77 <nomanrahim53@gmail.com>
2026-01-11 21:24:46 +0500

    header and home updated

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,13 +1,11 @@
 const Header = () => {
   return (
-    <header className="p-4 border-b border-gray-700 text-center">
-      <h1 className="text-2xl font-bold text-green-400">
-        GitGuide AI
-      </h1>
-      <p className="text-sm text-gray-400">
-        Your AI mentor for Git & GitHub
-      </p>
-    </header>
+    <div className="flex flex-col gap-2 justify-center items-center p-2 border-b border-gray-700">
+      <h1 className="text-xl font-bold">GitGuide AI</h1>
+      <h2>Ask any thing about git and github</h2>
+
+     
+    </div>
   );
 };
 
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,11 +1,23 @@
+import { useState } from "react";
 import Header from "../components/Header";
 import ChatBox from "../components/ChatBox";
 
+
 const Home = () => {
+  const [showReminder, setShowReminder] = useState(false);
+
   return (
-    <div className="h-screen bg-gray-900 text-white flex flex-col">
-      <Header />
-      <ChatBox />
+    <div className="h-screen bg-gray-900 text-white flex flex-col overflow-hidden">
+      
+      {/* Header */}
+      <Header  />
+
+
+      {/* Chat (scrolls independently) */}
+      <div className="flex-1 overflow-hidden">
+        <ChatBox />
+      </div>
+
     </div>
   );
 };
