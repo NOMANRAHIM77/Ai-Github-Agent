34d93c3d1e63b528afd3077c1f69fcf6cf72166d
NOMANRAHIM77 <nomanrahim53@gmail.com>
2026-01-28 22:59:09 +0500

    updated main.py file

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,24 +1,24 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+
 from app.routes.chat import router as chat_router
+
 
 app = FastAPI(title="GitGuide AI Backend")
 
-origins = [
-    "http://localhost:5173", # React Vite dev server port
-    "http://127.0.0.1:5173"
-]
-
+# CORS (important for frontend)
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins,  # React frontend origin
+    allow_origins=["*"],  # later you can restrict
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
 
+# Routes
 app.include_router(chat_router)
+
 
 @app.get("/")
 def root():
-    return {"status": "Backend running"}
+    return {"status": "Backend running successfully"}
